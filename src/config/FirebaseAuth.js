0ded08337e2a06f3065efe527b4ea5393aff6579
Harsh Kumar <hemenderkumar3000@gmail.com>
2024-08-08 03:49:34 +0530

    firebaseconfig2

--- src/config/FirebaseAuth.js
+++ src/config/FirebaseAuth.js
@@ -9,12 +9,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "apiKey":import.meta.enc.VITE_AOIKEY ,
-  "authDomain":import.meta.enc.VITE_AUTHDOMAIN ,
-  "projectId":import.meta.enc.VITE_PROJECTID ,
-  "storageBucket": import.meta.enc.VITE_STORAGEBUCKET,
-  "messagingSenderId": import.meta.enc.VITE_MESSAGINGSENDERID,
-  "appId": import.meta.enc.VITE_APPID
+  "apiKey":import.meta.env.VITE_APIKEY ,
+  "authDomain":import.meta.env.VITE_AUTHDOMAIN ,
+  "projectId":import.meta.env.VITE_PROJECTID ,
+  "storageBucket": import.meta.env.VITE_STORAGEBUCKET,
+  "messagingSenderId": import.meta.env.VITE_MESSAGINGSENDERID,
+  "appId": import.meta.env.VITE_APPID
 
 };
 
